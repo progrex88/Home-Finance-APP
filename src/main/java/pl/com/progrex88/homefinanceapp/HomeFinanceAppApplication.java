4197262448628b0ced60d406651d739685380ee6
progrex <progrex88@gmail.com>
2018-12-17 19:13:59 +0100

    first commit and push

---
+++ src/main/java/pl/com/progrex88/homefinanceapp/HomeFinanceAppApplication.java
@@ -0,0 +1,14 @@
+package pl.com.progrex88.homefinanceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeFinanceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeFinanceAppApplication.class, args);
+    }
+
+}
+
